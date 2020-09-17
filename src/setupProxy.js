8516e8d7fd68aaad098e56f6bbc13529c7cb3b89
Enrico Borba <enricozb@gmail.com>
2020-09-16 14:35:20 -1000

    github pages deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'https://remarkable-templates.herokuapp.com',
-      target: "http://0.0.0.0:8000",
+      target: 'https://remarkable-templates.herokuapp.com',
+      // target: "http://0.0.0.0:8000",
       changeOrigin: true,
       pathRewrite: {
         "^/api": ""
